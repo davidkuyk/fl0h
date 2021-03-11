e0e676ae137c90d147e13424f8914ab1d8b7248d
davidkuyk <davidkuyk@gmail.com>
2021-03-11 15:22:12 -0500

    Fix style.css import in loading.js

--- src/Components/loading.js
+++ src/Components/loading.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import './../style.css';
+import React from 'react';
+import '../style.css';
 
 const Loading = () => (
     <div class='water'></div>
